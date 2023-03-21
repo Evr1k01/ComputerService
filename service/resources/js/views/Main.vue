58caa874e93a4f0b144bd8a7a13ea7b75d4e25e6
Evr1k <maximu1258@gmail.com>
2023-03-21 18:14:53 +0100

    Neue Gestaltung der Hauptseite

--- service/resources/js/views/Main.vue
+++ service/resources/js/views/Main.vue
@@ -1,18 +1,23 @@
 <template>
-    <v-app-bar class="base__app-bar" flat>
+    <v-app-bar class="base__app-bar" border flat>
         <v-row justify="center">
             <v-col cols="11">
                 <v-row>
 
                     <v-col cols="2" md="4" lg="3">
                         <v-row>
-                            <v-img class="base__app-bar-logo align-self-center" :src="logo"></v-img>
-                            <p class="base__app-bar-logo-title">Computer-<span class="base__app-bar-logo-span">Service</span></p>
+                            <v-col cols="auto" class="align-self-center"><v-icon icon="mdi-apple-finder" size="x-large"></v-icon></v-col>
+                            <v-col cols="auto"><p class="base__app-bar-logo-title">ComputerService</p></v-col>
                         </v-row>
                     </v-col>
 
-                    <v-col cols="10" md="8" lg="9">
+                    <v-col cols="10" md="8" lg="9" class="align-self-center">
                         <v-row justify="end">
+                            <v-col cols="auto" align-self="center">
+                                <v-btn color="#262728" @click="dialogs.login = false; dialogs.registration = true" variant="flat" class="base__btn-info" append-icon="mdi-arrow-right-circle">
+                                    Registration
+                                </v-btn>
+                            </v-col>
                             <v-col cols="auto" align-self="center"><p><a class="base__app-bar-title" href="tel:+7 747 349 92 57">Anrufen</a></p></v-col>
                             <v-col cols="auto" align-self="center"><p><a class="base__app-bar-title" href="mailto:maximu17@mail.ru">@email</a></p></v-col>
                         </v-row>
@@ -23,69 +28,34 @@
         </v-row>
     </v-app-bar>
 
-    <v-main scrollable>
+    <v-main scrollable class="base">
         <v-row justify="center">
             <v-col cols="11">
                 <v-row>
-                    <v-col cols="12" md="7" lg="8">
+                    <v-col cols="12" md="7" lg="6">
+                        <p class="base__title-head mt-3">Unsere Bedingungen.</p>
 
-                        <v-card class="base__container" color="#eec71d" flat>
-                            <v-row justify="center">
-                                <v-col cols="12" sm="10" align-self="center">
-                                    <p class="base__title-one">Wir bieten optimale Wartung für bescheidene Preise an!</p>
-                                    <v-row justify="center" class="justify-md-start mt-3">
-                                        <v-col cols="10" sm="8" md="6">
-                                            <v-btn flat color="#879192" class="base__btn-info" size="large" append-icon="mdi mdi-information-outline">
-                                                Mehr erfahren
-                                            </v-btn>
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                        </v-card>
+                        <p class="base__title-text">Auf den ersten Blick scheint unsere
+                            Werkstatt gewöhnlich zu sein, aber in Wirklichkeit
+                            werden Sie positiv überrascht sein.
+                        </p>
 
-                        <v-card class="base__container" color="#9fcdff" flat>
-                            <v-row justify="center">
-                                <v-col cols="12" sm="10" align-self="center">
-                                    <p class="base__title-one">Sie können sich anmelden, und alles selbst ausprobieren</p>
+                        <v-btn
+                            class="base__btn-info"
+                            color="#262728"
+                            @click="dialogs.registration = false; dialogs.login = true"
+                        >
+                            Sich Anmelden
+                        </v-btn>
 
-                                    <v-row justify="center" class="mt-3">
-                                        <v-col cols="10" sm="6" align-self="center" class="text-md-start">
-                                            <v-btn flat color="success" variant="flat" class="base__btn-info" size="large" append-icon="mdi mdi-account-check-outline" @click="dialogs.login = true; dialogs.registration = false">
-                                                Anmelden
-                                            </v-btn>
-                                        </v-col>
-
-                                        <v-col cols="10"  sm="6" align-self="center"  class="text-md-end">
-                                            <v-btn flat color="#a12a4b" variant="flat" class="base__btn-info" size="large" append-icon="mdi mdi-account-alert-outline" @click="dialogs.registration = true; dialogs.login = false">
-                                                Registrieren
-                                            </v-btn>
-                                        </v-col>
-                                    </v-row>
-
-                                </v-col>
-                            </v-row>
-                        </v-card>
-
-                        <v-card class="base__container" color="#6cd759" flat>
-                            <v-row justify="center">
-                                <v-col cols="12" sm="10" align-self="center">
-                                    <p class="base__title-one">Falls Sie genug Zeit haben, könnten Sie unsere Beurteilungen lesen</p>
-                                    <v-row justify="center" class="justify-md-start mt-3">
-                                        <v-col cols="10" md="6">
-                                            <v-btn flat color="#879192" class="base__btn-info" size="large" append-icon="mdi mdi-information-outline">
-                                                Lesen
-                                            </v-btn>
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                        </v-card>
                     </v-col>
 
 
-                    <v-col cols="12" md="5" lg="4">
-                        <div class="base__registration_container">
+                    <v-col cols="12" md="5" lg="6">
+                        <v-row justify="end">
+                            <v-col cols="8">
+
+                        <div class="base__registration_container mt-3">
                             <v-card class="base__registration_container-card" v-show="dialogs.registration">
                                 <v-card-title class="base__registration_container-card-title text-center">Registration</v-card-title>
                                 <v-form
@@ -94,8 +64,9 @@
                                 >
                                     <!-- Name -->
                                     <v-text-field
+                                        class="base__registration_container-card-input"
                                         prepend-inner-icon="mdi-account-box-outline"
-                                        variant="underlined"
+                                        variant="outlined"
                                         placeholder="Bspw. Maxim"
                                         label="Name"
                                         clearable
@@ -104,7 +75,7 @@
 
                                     <!-- Email -->
                                     <v-text-field
-                                        variant="underlined"
+                                        variant="outlined"
                                         placeholder="Bspw. xxx@gmail.com"
                                         prepend-inner-icon="mdi-email-outline"
                                         label="Email"
@@ -116,7 +87,7 @@
                                     <!-- Passwort -->
                                     <v-text-field
                                         prepend-inner-icon="mdi-lock-outline"
-                                        variant="underlined"
+                                        variant="outlined"
                                         label="Passwort"
                                         type="password"
                                         clearable
@@ -126,7 +97,7 @@
                                     <!-- Wiederholung -->
                                     <v-text-field
                                         prepend-inner-icon="mdi-lock-outline"
-                                        variant="underlined"
+                                        variant="outlined"
                                         label="Passwort (Wiederholung)"
                                         type="password"
                                         clearable
@@ -137,9 +108,9 @@
                                         <v-col cols="12" md="10">
                                             <v-btn
                                                 class="base__registration_container-card-btn"
-                                                variant="outlined"
+                                                variant="flat"
                                                 size="x-large"
-                                                color="#a12a4b"
+                                                color="#ffcd21"
                                             >
                                                 Bestätigen
                                             </v-btn>
@@ -180,9 +151,9 @@
                                         <v-col cols="12" md="10">
                                             <v-btn
                                                 class="base__registration_container-card-btn"
-                                                variant="outlined"
+                                                variant="flat"
                                                 size="x-large"
-                                                color="#a12a4b"
+                                                color="#ffcd21"
                                             >
                                                 Einloggen
                                             </v-btn>
@@ -192,6 +163,9 @@
                             </v-card>
 
                         </div>
+
+                            </v-col>
+                        </v-row>
                     </v-col>
                 </v-row>
             </v-col>
